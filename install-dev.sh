13685ab88643dbb4c786cdca9e944ba91cdb6587
Simone Gaiarin <simgunz@gmail.com>
2011-12-20 18:03:43 +0100

    updated install process

--- install-dev.sh
+++ install-dev.sh
@@ -6,8 +6,9 @@ if [ ! -d ${buildDir} ]
 then mkdir build || exit
 fi
 
-
 cd ${buildDir} || exit
 cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` ../src || exit
 make || exit
 sudo make install || exit
+
+sudo cp ../testfile/timekpr.conf /etc/timekpr/timekpr.conf
